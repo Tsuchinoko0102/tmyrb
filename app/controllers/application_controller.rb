4e2aa48e4878d02b688414fc3dabab75214bf3ee
Tsuchinoko0102 <tsuchinoko.com@gmail.com>
2020-10-05 16:36:34 +0900

    ユーザー新規登録完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+      keys:[:nickname, :prefecture_id, :works, :genre_id, :likes_author,
+        :likes_book, :appeal_text])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_10_04_090412) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.integer "prefecture_id"
