f0be5ae8a1e038d33b500ba56c1f1b14016b13cd
Tsuchinoko0102 <tsuchinoko.com@gmail.com>
2020-10-12 18:58:53 +0900

    authenticate_user!メソッド追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   before_action :session_delete, only: [:index, :show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, except: :index
 
   def index
   end
