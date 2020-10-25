8fba6a03f602c22704dec6c7b9fe5e49b7a7d808
Tsuchinoko0102 <tsuchinoko.com@gmail.com>
2020-10-26 01:54:18 +0900

    application.jsでrequire chartkick / chart.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,9 @@ require('data-confirm-modal')
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-import Rails from 'rails-ujs';
-Rails.start();
+require("chartkick")
+require("chart.js")
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
