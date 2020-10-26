6b772737a26bb8043d50bed8849fea20ef2e7ba0
Tsuchinoko0102 <tsuchinoko.com@gmail.com>
2020-10-26 19:17:10 +0900

    pie-chart データ取り出し成功

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   
   def show
     @books = Book.where(user_id: current_user.id)
+    @pie = User.chart_data(params[:id])
+    binding.pry
   end
 
   def edit
