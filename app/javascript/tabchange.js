4d4e5eda682129532ae6ba064f7716b3100e4954
Tsuchinoko0102 <tsuchinoko.com@gmail.com>
2020-11-19 18:18:43 +0900

    tabchangeにコメントアウトを追加

--- app/javascript/tabchange.js
+++ app/javascript/tabchange.js
@@ -1,18 +1,25 @@
 function tabchange(){
+  // イベント発火対象をtabs変数に規定
   const tabs = document.querySelectorAll(".tab-link");
+  // tabsのいずれかがclickされた際に、Ajax通信発生
   tabs.forEach(function(tab) {
     $(tab).click(function(){
+      // clickされたタブのIDをコントローラに送信するため、tabIdを定義
       let tabId = tab.getAttribute("data-id")
       $.ajax({
+        // URLはtabchangeアクションを動かすために設定
         url: `/tab/${tabId}`,
         type: "GET",
+        // 選択されたタブの文字列をコントローラで照会するため、contentとしてデータ送信
         data: {content : $(tab).text()},
         datatype: "html",
         })
+        // 成功した場合は、元のother-booksを削除して、AjaxのresponseをHTML要素として描画
         .done(function(response){
           $('.other-books').children().remove();
           $('.other-books').html(response);
         })
+        // 失敗した場合は、エラーメッセージを表示する
         .fail(function(){
           alert('エラーが発生しました');
         })
