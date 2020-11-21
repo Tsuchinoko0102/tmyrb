f23beddb7cbf27775cf7ad7e5fea18a06ffaa146
Tsuchinoko0102 <tsuchinoko.com@gmail.com>
2020-11-22 01:25:43 +0900

    genreselect.jsにコメントアウト追記

--- app/javascript/genreselect.js
+++ app/javascript/genreselect.js
@@ -1,18 +1,21 @@
 function genreChange() {
+  // templates/partial からid="category"の要素を取得
   const catTarget = $("#category");
   for (let i = 0; i < catTarget.length; i++){
+    // catTargetのいずれかの項目が選択された時、その項目のvalue（数値）を所得しcatNumへ代入
     $(catTarget[i]).change(function() {
       let catNum = catTarget[i].value;
-      console.log(catNum);
+    // 取得したcatNumを元にAjaxでgenre_selectアクションへ移行し、HTMLでのレスポンスを要求
       $.ajax({
         url: `/cat/${Number(catNum)}`,
         type: "GET",
         data: {num: catNum},
         datatype: "html"
       })
+      // 成功した場合は、responseされたpartialを描画する
       .done(function(response){
-        // const selectBox = $.parseHTML(response);
         const target = document.getElementById("genre");
+      // 既に描画されたpartialが存在する場合は、先にそれを削除する
         const removeDiv = document.getElementById("genre_id");
         if (removeDiv != null) {
           removeDiv.remove();
