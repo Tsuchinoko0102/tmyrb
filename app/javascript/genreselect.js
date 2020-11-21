d4d01f20e29db45ed2a41970b63a6aa7472ddd7e
Tsuchinoko0102 <tsuchinoko.com@gmail.com>
2020-11-21 21:06:03 +0900

    genreChange.js作成

---
+++ app/javascript/genreselect.js
@@ -0,0 +1,26 @@
+function genreChange() {
+  const catTarget = $("#category");
+  for (let i = 0; i < catTarget.length; i++){
+    $(catTarget[i]).change(function() {
+      let catNum = catTarget[i].value;
+      console.log(catNum);
+      $.ajax({
+        url: `/cat/${Number(catNum)}`,
+        type: "GET",
+        data: {num: catNum},
+        datatype: "html"
+      })
+      .done(function(response){
+        // const selectBox = $.parseHTML(response);
+        const target = document.getElementById("genre");
+        target.insertAdjacentHTML("beforeend", response);
+      })
+      // 失敗した場合は、エラーメッセージを表示する
+      .fail(function(){
+        alert('エラーが発生しました');
+      })
+    });
+  };
+};
+
+window.addEventListener("load", genreChange);
