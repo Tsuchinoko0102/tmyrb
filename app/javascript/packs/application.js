b5e8ecb90090a35b5c9cb5801a8ce34c6d4ce8c4
Tsuchinoko0102 <tsuchinoko.com@gmail.com>
2020-11-24 00:17:46 +0900

    CSS・画像の管理をwebpackerに変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 require('jquery') 
 require("chartkick")
 require("chart.js")
+import '../css/application.scss'
 require("../tab")
 require("../tabchange")
 require("../genreselect")
@@ -19,5 +20,5 @@ require("../genreselect")
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
