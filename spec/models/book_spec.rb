150136d6d8e70e2b7c3e77fafd59137eb8dbf3a0
Tsuchinoko0102 <tsuchinoko.com@gmail.com>
2020-12-08 19:02:47 +0900

    Booksモデルexample抽出

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Bookモデル登録のテスト" do
+    # 正常系
+    it "すべての項目が入力されていたら登録できる" do
+
+    end
+
+    # 異常系
+    it "titleが空欄だと登録できない" do
+
+    end
+
+    it "authorが空欄だと登録できない" do
+
+    end
+
+    it "genre_idが空欄だと登録できない" do
+
+    end
+
+    it "genre_idが1の場合は登録できない" do
+
+    end
+
+    it "leadが空欄だと登録できない" do
+
+    end
+
+    it "contentが空欄だと登録できない" do
+
+    end
+
+    it "rank_idが空欄だと登録できない" do
+
+    end
+
+    it "rank_idが1の場合は登録できない" do
+
+    end
+
+    it "publishが空欄だと登録できない" do
+
+    end
+
+    it "userが空欄だと登録できない" do
+
+    end
+
+    it "imageが空欄だと登録できない" do
+
+    end
+
+    it "urlが空欄だと登録できない" do
+      
+    end
+  end
 end
